915487bd88f7634c7aad9dcb5a0a0e1f9e43a804
johnnyblaze993 <107284554+johnnyblaze993@users.noreply.github.com>
2023-01-14 12:17:28 -0500

    styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,15 +7,15 @@ const NavBar = () => {
       <div className={styles.left}>
         <div>logo</div>
         <div className={styles.links}>
-          <div>Home</div>
-          <div>Our Plans</div>
-          <div>Bootcamps</div>
-          <div>Hours</div>
+          <button className={styles.link}>Home</button>
+          <button className={styles.link}>Our Plans</button>
+          <button className={styles.link}>Bootcamps</button>
+          <button className={styles.link}>Hours</button>
         </div>
       </div>
       <div className={styles.right}>
-        <div>Sign in</div>
-        <div>Become a member</div>
+        <div className={styles.signin}>Sign in</div>
+        <button className={styles.register}>Become a member</button>
       </div>
     </div>
   );
