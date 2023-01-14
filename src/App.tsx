37e7bc2d2b9276de221fa8bec21b62e2216f1ce6
johnnyblaze993 <107284554+johnnyblaze993@users.noreply.github.com>
2023-01-13 20:24:57 -0500

    npm i

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-// import React from "react";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <h1>Input project</h1>
+      <h1>React + TypeScript + Webpack</h1>
     </div>
   );
 }
